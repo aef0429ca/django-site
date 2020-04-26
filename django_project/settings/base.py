1d3c532f1e3f45164e23641552b585d91ee6ca68
Anders <anders@agenteimovel.com.br>
2020-04-26 13:56:51 +0200

    fixing dev and prod db host

--- django_project/settings/base.py
+++ django_project/settings/base.py
@@ -67,30 +67,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'django_project.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/3.0/ref/settings/#databases
-
-'''
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-'''
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': 'SKAL123vit',
-        'HOST': 'db',
-        'PORT': 5432,
-    }
-}
-
-
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
 
--- django_project/settings/prod.py
+++ django_project/settings/prod.py
@@ -4,3 +4,16 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['49.12.12.34', 'www.endirio.com', 'endirio.com']
 
+# Database
+# https://docs.djangoproject.com/en/3.0/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'SKAL123vit',
+        'HOST': 'localhost',
+        'PORT': 5432,
+    }
+}
