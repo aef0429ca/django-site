d6d01a2e3dde4a861cc352ba9cff635088a1f6cb
Anders <anders@agenteimovel.com.br>
2020-04-26 14:16:08 +0200

    setting host to 127.0.0.1

--- django_project/settings/prod.py
+++ django_project/settings/prod.py
@@ -4,16 +4,13 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['49.12.12.34', 'www.endirio.com', 'endirio.com']
 
-# Database
-# https://docs.djangoproject.com/en/3.0/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'postgres',
         'USER': 'postgres',
         'PASSWORD': 'SKAL123vit',
-        'HOST': 'localhost',
+        'HOST': '127.0.0.1',
         'PORT': 5432,
     }
 }
