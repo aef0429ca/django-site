8167afb1f1bf70b8ef4447c9f127516f8f87b328
Anders <anders@agenteimovel.com.br>
2020-05-06 22:22:34 +0200

    Added page for critical tags

--- xmltools/urls.py
+++ xmltools/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path, re_path
+from django.views.generic import TemplateView
 
 from . import views
 
@@ -9,6 +10,6 @@ urlpatterns = [
     path('xml_fetch.html', views.xml_fetch, name='xml_fetch'),
     path('xml_format_test/<int:pk>', views.xml_format_test, name='xml_format_test'),
     path('xml_analyze/<int:pk>', views.xml_analyze, name='xml_analyze'),
-    # path('xml_analyze.html', views.xml_analyze, name='xml_analyze'),
     path('xml_profile/<int:pk>', views.xml_profile, name='xml_profile'),
+    path('xml_tags.html', TemplateView.as_view(template_name='xmltools/xml_tags.html'), name='xml_tags'),
 ]
